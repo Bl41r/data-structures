388ceaa48667df4c9e42720e239cb59bb78f8593
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-08-16 15:49:49 -0700

    Stack finished. Requirements added.

--- src/stack.py
+++ src/stack.py
@@ -5,12 +5,21 @@
 from linked_list import LinkedList
 
 
-class Stack(object, idx=None):
-    def __init__(self, idx):
-        self._container = LinkedList(idx)
+class Stack(object):
+    '''
+    This is our Stack class and it's associated methods.
+    The Stack class accepts an optional iterable as a parameter.
+    '''
+
+    def __init__(self, idx=None):
+        self._stack = LinkedList(idx)
+
+    def __repr__(self):
+        s = self._stack.__repr__()
+        return str(s)
 
     def push(self, val):
-        self._container.push(val)
+        self._stack.push(val)
 
     def pop(self):
-        self._container.pop()
+        self._stack.pop()
