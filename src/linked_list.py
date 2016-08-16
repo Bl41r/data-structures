533ff5f41a20854e112f19822dcef41b7040c95e
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-08-16 16:05:28 -0700

    sync issues

--- src/linked_list.py
+++ src/linked_list.py
@@ -6,24 +6,32 @@ class Node(object):
     def __init__(self, data, next_node=None):
         """Initialize the Node instance."""
         self.data = data
-        self.next_node = next_node
+        if next_node is not None:
+            self.next_node = next_node
 
     def __repr__(self):
         """Display the data in this node."""
-        return str(self.data) + u', ' + str(self.next_node)
+        try:
+            return str(self.data) + u', ' + str(self.next_node)
+        except AttributeError:
+            return str(self.data)
 
 
 class LinkedList(object):
 
-    def __init__(self, node_list):
+    def __init__(self, params=None):
         """Initialize the linked list instance."""
 
         self.head = None
-        self.tail = Node(node_list[0])
         self.length = 0
-        for node in node_list:
-            self.length += 1
-            self.head = Node(node, self.head)
+
+        try:
+            for node in params:
+                self.length += 1
+                self.push(node)
+        except TypeError:
+            self.push(params)
+            self.length = 1
 
     def __repr__(self):
         """Display the linked list."""
@@ -38,15 +46,22 @@ class LinkedList(object):
 
     def push(self, val):
         """Insert the value val at the head of the list."""
-        self.head = Node(val, self.head)
-        self.length += 1
+        if self.head is not None:
+            self.head = Node(val, self.head)
+            self.length += 1
+        else:
+            self.head = Node(val)
+            self.length += 1
 
     def pop(self):
         """Pop the first value off the head of the list and return it."""
-        popped_node = self.head
-        self.head = self.head.next_node
-        self.length -= 1
-        return popped_node.data
+        if self.length > 0:
+            popped_node = self.head
+            self.head = self.head.next_node
+            self.length -= 1
+            return popped_node.data
+        else:
+            return None
 
     def size(self):
         """Return the length of the list."""
