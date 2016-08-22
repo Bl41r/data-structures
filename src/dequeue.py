4541e64a3ad19ebdcc99dc9b59de67b6724f51b3
David Smith <dbsmith.dbs83@gmail.com>
2016-08-22 14:45:06 -0700

    methods made

--- src/dequeue.py
+++ src/dequeue.py
@@ -8,27 +8,29 @@ class Deque(object):
     """
 
     def __init__(self, iter=None):
-        self._queue = Dll(iter)
+        self._deque = Dll(iter)
         self.length = self.size()
 
     def __repr__(self):
-        s = self._queue.__repr__()
+        s = self._deque.__repr__()
         return str(s)
 
     def appendleft(self, val):      # this was enqueue(val)
         """Push value onto the deque head."""
-        self._queue.push(val)
+        self._deque.push(val)
         self.length += 1
         return self
 
     def append(self, val):
         """Append value to end of deque."""
-        pass
+        self._deque.append(val)
+        self.length += 1
+        return self
 
     def pop(self):  # this was dequeue()
         """Remove and return value from the deque and return it."""
         try:
-            val = self._queue.shift()
+            val = self._deque.shift()
             self.length -= 1
             return val
         except AttributeError:
@@ -37,18 +39,28 @@ class Deque(object):
 
     def popleft(self):
         """Remove and return value from front of deque."""
+        try:
+            val = self._deque.pop()
+            self.length -= 1
+            return val
+        except AttributeError:
+            raise IndexError('Cannot pop an empty queue.')
+            pass
 
     def peek(self):
         """Peek at next value without dequeing it."""
-        if self._queue.length > 0:
-            return self._queue.tail.data
+        if self._deque.length > 0:
+            return self._deque.tail.data
         else:
             return None
 
     def peekleft(self):
         """Peek at front of dequeue without dequeing it."""
-        pass
+        if self._deque.length > 0:
+            return self._deque.head.data
+        else:
+            return None
 
     def size(self):
         """Return size of deque."""
-        return self._queue.length
+        return self._deque.length
