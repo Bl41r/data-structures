9199462ae39e7e931eda1b041a35bbc1acc9ae43
Jeff Torres <jeffrey.n.torres@gmail.com>
2016-08-21 23:17:11 -0700

    tried to fix testing. no dice

---
+++ src/test_queue.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""Test of dbl_lnk_lst.py."""
+
+import pytest
+from dbl_lnk_lst import Dll
+from queue import Queue
+
+# format:
+# (list of datas,
+# length,
+# head data,
+# shift)
+
+
+TEST_DATAS = [
+    ([5, 4, 3, 2, 1], 5, 1, 1),
+    (['z', 'y', 'x', 'w', 'v', 'u'], 6, 'u', 'u'),
+    ([], 0, None, None)
+]
+
+TEST_LISTS = [
+    (Dll(data[0]),
+        data[1],
+        data[2],
+        data[3]) for data in TEST_DATAS]
+
+
+@pytest.mark.parametrize(
+    'list_data, length, head_data, shift', TEST_DATAS
+    )
+def test_data(list_data, length, head_data, shift):
+    q = Queue(list_data)
+    print(q)
+    assert q.length == length
+    assert q._queue.head.data == head_data
+    if q._queue.head.data is not None:
+        assert q._queue.head.next_node.data == q._queue.head.next_node.data
+
+
+@pytest.mark.parametrize(
+    'list_data, length, head_data, shift', TEST_DATAS
+    )
+def test_size(list_data, length, head_data, shift):
+    q = Queue(list_data)
+    assert q.size() == length
+
+
+@pytest.mark.parametrize(
+    'list_data, length, head_data, shift', TEST_DATAS
+    )
+def test_enqueue(list_data, length, head_data, shift):
+    q = Queue(list_data)
+    assert q.enqueue(6).peek() == q.peek()
+
+
+@pytest.mark.parametrize(
+    'list_data, length, head_data, shift', TEST_DATAS
+    )
+def test_dequeue(list_data, length, head_data, shift):
+    q = Queue(list_data)
+    if q is not None:
+        assert q.dequeue() == q._queue.head
+    else:
+        q.dequeue() == IndexError
+
+
+@pytest.mark.parametrize(
+    'list_data, length, head_data, shift', TEST_DATAS
+    )
+def test_peek(list_data, length, head_data, shift):
+    q = Queue(list_data)
+    if q:
+        assert q.peek() == q._queue.tail
