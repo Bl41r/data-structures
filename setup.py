e418dad72127fdefe62718230a38c9a6c7198544
David Smith <dbsmith.dbs83@gmail.com>
2016-08-25 13:06:08 -0700

    dll

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name="linked-list",
-    description="Implementation of a linked list",
+    name="double linked-list",
+    description="Implementation of a double linked list",
     version='0.1.0',
     author="David Smith, Jeff Torres",
     author_email="dbsmith.dbs83@gmail.com, jeffrey.n.torres@gmail.com",
     license='MIT',
-    py_modules=['linked-list'],
+    py_modules=['dbl_lnk_lst'],
     package_dir={'': 'src'},
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-cov', 'tox']},
