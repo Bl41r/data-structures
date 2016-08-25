88cead4be75d39a2266e0ffb744313ae82dbe9ef
David Smith <dbsmith.dbs83@gmail.com>
2016-08-25 00:33:22 -0700

    ll docstrings linter stuff

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,7 +1,10 @@
-"""Implementation of a linked-list data type in Python."""
+"""Implementation of a link-list data type in Python."""
+
+from __future__ import unicode_literals
 
 
 class Node(object):
+    """Node class with data and next_node pointer.  Init with value."""
 
     def __init__(self, data, next_node=None):
         """Initialize the Node instance."""
@@ -17,65 +20,77 @@ class Node(object):
 
 
 class LinkedList(object):
+    """Linked List data structure.  Takes in an iterable."""
 
     def __init__(self, params=None):
         """Initialize the linked list instance."""
-        self.head = Node(None)
-        self.length = 0
+        self.head = None
 
-        if hasattr(params, '__iter__'):
-            for node in params:
-                self.push(node)
-        elif params:
-            self.push(params)
+        try:
+            for value in params:
+                self.push(value)
+        except TypeError:
+            if params is not None:
+                raise TypeError('This function requires an iterable value')
 
     def __repr__(self):
         """Display the linked list."""
         return u'(' + str(self.head) + u')'
 
     def __len__(self):
-        return self.length
+        """Return length of list."""
+        return self.size()
 
     def display(self):
-        """Return a unicode string representing the linked list as if it were a Python tuple."""
+        """Return a unicode string representing the list as if it were a Python tuple."""
         return u'(' + str(self.head) + u')'
 
     def push(self, val):
         """Insert the value val at the head of the list."""
         self.head = Node(val, self.head)
-        self.length += 1
 
     def pop(self):
-        """Pop the first value off the head of the linked list and return it."""
-        if self.length > 0:
-            popped_node = self.head
+        """Pop the first value off the head of the list and return it."""
+        popped_node = self.head
+        try:
             self.head = self.head.next_node
-            self.length -= 1
             return popped_node.data
-        else:
+        except AttributeError:
+            pass
             return None
 
     def size(self):
         """Return the length of the list."""
-        return self.length
+        current = self.head
+        inc = 0
+        while True:
+            try:
+                current = current.next_node
+            except AttributeError:
+                return inc
+            inc += 1
 
     def search(self, val):
         """Return the node containing val in the list, if exists, else None."""
         current = self.head
-        while current.data is not None:
+        while current is not None:
             if current.data == val:
-                return current
-            elif current.next_node is None:
-                return None
+                break
             current = current.next_node
+        return current
 
     def remove(self, node):
-        """Remove the given node from the list, wherever it might be."""
-        if node.next_node is not None:
-            node.data = node.next_node.data
-            node.next_node = node.next_node.next_node
-        else:
-            node.data = None
-            node.next_node = None
+        """Remove the given node from the list, from any point in the list."""
+        parent = self.head
+        if parent.next_node == node.next_node:
+            self.head = parent.next_node
+            parent.next_node = None
+            return
 
-        self.length -= 1
+        while parent.next_node != node:
+            parent = parent.next_node
+
+        try:
+            parent.next_node = parent.next_node.next_node
+        except AttributeError:
+            parent.next_node = None
