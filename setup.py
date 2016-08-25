520ca38ae6554f4b9d669d2e31697a1435514a74
David Smith <dbsmith.dbs83@gmail.com>
2016-08-25 13:05:15 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name="linked-list",
-    description="Implementation of a linked list",
+    name="stack",
+    description="Implementation of a stack",
     version='0.1.0',
     author="David Smith, Jeff Torres",
     author_email="dbsmith.dbs83@gmail.com, jeffrey.n.torres@gmail.com",
     license='MIT',
-    py_modules=['linked-list'],
+    py_modules=['stack'],
     package_dir={'': 'src'},
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-cov', 'tox']},
