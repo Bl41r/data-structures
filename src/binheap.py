f7881530c0817148eb7b5fa0107764e07e61ab9d
David Smith <dbsmith.dbs83@gmail.com>
2016-08-26 02:08:15 -0700

    helper functions

--- src/binheap.py
+++ src/binheap.py
@@ -17,9 +17,9 @@ class MinHeap(object):
         try:
             self.heap = sorted(heap)
             for num in heap:
-                self.push(int(num))
+                self.heap.append(int(num))
         except (ValueError, TypeError):
-            raise TypeError('Heap must contain integers only.')
+            raise TypeError('Heap must contain only integers.')
 
     def __repr__(self):
         """Display the heap in list form."""
@@ -27,8 +27,36 @@ class MinHeap(object):
 
     def push(self, val):
         """Push an integer onto the heap."""
-        pass
+        try:
+            self.head.append(int(val))
+        except TypeError:
+            raise TypeError('Must push an integer value.')
 
     def pop(self):
         """Pop the root of the tree and return the value."""
         pass
+
+    def swap(self, a, b):
+        """Swap 2 values in heap."""
+        self.heap[a], self.heap[b] = self.heap[b], self.heap[a]
+
+    def parent_index(self, child_idx):
+        """Return index of parent. Negative 1 indicates idx is root."""
+        return int(child_idx + 1 // 2) - 1
+
+    def first_child(self, parent_idx):
+        """Return index of left child."""
+        return 2 * parent_idx + 1
+
+"""
+             0
+          1     3
+        2   5  4  7  ---> Pop   1
+                               0 3
+                              2 54 7 --->
+                                            1
+                                           2  3
+                                          0 54 7 ---> 1
+                                                     2 3
+                                                    54 7
+"""
