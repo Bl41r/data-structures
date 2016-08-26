0f5a62d2c192174dab1bc888b4861dc267673112
David Smith <dbsmith.dbs83@gmail.com>
2016-08-25 23:11:22 -0700

    skeleton

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # data-structures
 Repo that holds implementations of a variety of data structures
 
-This branch features the binary heap
+This branch features the priority queue. A Priority Queue is similar to a queue, except that in addition to a value, each item in the queue has a “priority”. When you pop an item off of the queue, you always get the highest priority item.
 
 #### methods
-.push(): puts a new value into the heap, maintaining the heap property.
-.pop(): removes the “top” value in the heap, maintaining the heap property.
+.insert(item): inserts an item into the queue.
+.pop(): removes the most important item from the queue.
+.peek(): returns the most important item without removing it from the queue.
 
---
+++ src/priorityq.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""This is a module for a priority queue.
+
+A Priority Queue is similar to a queue, except that in addition to a
+value, each item in the queue has a “priority”. When you pop an item
+off of the queue, you always get the highest priority item.
+"""
+
+
+class PriorityQueue(object):
+    """Min heap class with push and pop methods."""
+
+    def __init__(self, queue=None):
+        """Initialize the heap with optional heap(list) of integers."""
+        self.queue = queue
+
+    def __repr__(self):
+        """Display the heap in list form."""
+        return repr(self.queue)
+
+    def insert(self, n):
+        """Insert an item into the queue."""
+        pass
+
+    def pop(self):
+        """Pop the exit-node of the queue and return the value."""
+        pass
+
+    def peek(self):
+        """Peek at the next item to be popped."""
