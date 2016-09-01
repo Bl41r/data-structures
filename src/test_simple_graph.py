56a01739e76fd448cee6d3ac30acb15ca3ecc2de
ilikesounds <email_id>
2016-09-01 14:27:38 -0700

    Node test added

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -320,3 +320,20 @@ def test_adjacent_non_node(sg):
     sg.graph.add_node(a)
     with pytest.raises(TypeError):
         assert sg.graph.adjacent(a, strung_input)
+
+
+def test_sg_nodes_in_graph(sg):
+    from simple_graph import Node
+    strung_input = str(sg.input_val)
+    a = Node(strung_input)
+    b = Node(strung_input * 2)
+    c = Node(strung_input * 5)
+    d = Node(strung_input * 3)
+    e = Node(strung_input * 4)
+    sg.graph.add_node(a)
+    sg.graph.add_node(c)
+    sg.graph.add_node(d)
+    sg.graph.add_node(e)
+    sg.graph.add_node(b)
+    result = sg.graph.nodes()
+    assert len(result) == 5
