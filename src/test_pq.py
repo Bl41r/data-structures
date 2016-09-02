1c5d26a1dbd3bca62ccf4249740d983f65c1fae8
ilikesounds <email_id>
2016-09-01 18:14:44 -0700

    tests finished

--- src/test_pq.py
+++ src/test_pq.py
@@ -36,7 +36,7 @@ TEST_CASES = EDGE_CASES + INT_CASES + STR_CASES
 
 MyPQFix = namedtuple(
     'pqFixture',
-    ('pqueue', 'input_val', 'len_int_list', 'type_err')
+    ('pqueue', 'input_val', 'len_int_list', 'type_err', 'int_list_priority')
 )
 
 
@@ -60,7 +60,14 @@ def pq(request):
             pass
     len_int_list = len(int_list_priority)
     int_list_priority = sorted(int_list_priority)
-    return MyPQFix(pqueue, input_val, len_int_list, type_err)
+    return MyPQFix(pqueue,
+                   input_val,
+                   len_int_list,
+                   type_err,
+                   int_list_priority
+                   )
+
+# Test PNode creation
 
 
 def test_node_init(pq):
@@ -72,23 +79,99 @@ def test_node_init(pq):
         with pytest.raises(ValueError):
             PNode(pq.input_val, pq.input_val)
 
+# Test PriorityQueue public functions
+
+
+def test_insert_non_list_error(pq):
+    """
+    Confirm that attempting to initialize a PriorityQueue with anything but a
+    a list throws a TypeError
+    """
+    with pytest.raises(TypeError):
+        pq.pqueue(pq.input_val)
+
+
+def test_instantiate_list_non_PNodes(pq):
+    """
+    Confirm that attempting to initialize a PriorityQueue with anything but a
+    a list of PNodes throws a TypeError
+    """
+    with pytest.raises(TypeError):
+        pq.pqueue(pq.int_list_priority)
+
 
 def test_insert_nonnode(pq):
+    """
+    Confirm that attempting to insert anything buy a PNode type
+    throws a TypeError
+    """
     with pytest.raises(TypeError):
         pq.pqueue.insert(pq.input_val)
 
 
 def test_insert_node(pq):
+    """
+    Confirm that the insert functions adds PNodes to the PriorityQueue and
+    sorts them properly regardless of order of insert.
+    """
     from priorityq import PNode
-    if pq.input_val is int():
-        a = PNode(pq.input_val)
-        b = PNode(pq.input_val * 2)
-        c = PNode(pq.input_val * 3)
-        d = PNode(pq.input_val * 4)
-        e = PNode(pq.input_val * 5)
-    pq.pqueue.insert(e)
-    pq.pqueue.insert(c)
-    pq.pqueue.insert(a)
-    pq.pqueue.insert(d)
-    pq.pqueue.insert(b)
-    assert pq.pqueue.heap[0] == a
+    if type(pq.input_val) is int:
+        a = PNode('a', pq.input_val)
+        b = PNode('b', pq.input_val * 2)
+        c = PNode('c', pq.input_val * 3)
+        d = PNode('d', pq.input_val * 4)
+        e = PNode('e', pq.input_val * 5)
+        pq.pqueue.insert(e)
+        pq.pqueue.insert(c)
+        pq.pqueue.insert(a)
+        pq.pqueue.insert(d)
+        pq.pqueue.insert(b)
+        assert pq.pqueue.heap[0] == a
+
+
+def test_empty_pqueue(pq):
+    with pytest.raises(IndexError):
+        pq.pqueue.pop()
+
+
+def test_pop_node(pq):
+    """
+    Confirm that the insert functions adds PNodes to the PriorityQueue and
+    sorts them properly regardless of order of insert.
+    """
+    from priorityq import PNode
+    if type(pq.input_val) is int:
+        a = PNode('a', pq.input_val)
+        b = PNode('b', pq.input_val * 2)
+        c = PNode('c', pq.input_val * 3)
+        d = PNode('d', pq.input_val * 4)
+        e = PNode('e', pq.input_val * 5)
+        pq.pqueue.insert(e)
+        pq.pqueue.insert(c)
+        pq.pqueue.insert(a)
+        pq.pqueue.insert(d)
+        pq.pqueue.insert(b)
+        assert pq.pqueue.pop() == a
+
+
+def test_peek_empty_error(pq):
+    pq.pqueue.peek() is None
+
+
+def test_peek(pq):
+    """
+    Confirm that the peek function returns the highest priority node.
+    """
+    from priorityq import PNode
+    if type(pq.input_val) is int:
+        a = PNode('a', pq.input_val)
+        b = PNode('b', pq.input_val * 2)
+        c = PNode('c', pq.input_val * 3)
+        d = PNode('d', pq.input_val * 4)
+        e = PNode('e', pq.input_val * 5)
+        pq.pqueue.insert(e)
+        pq.pqueue.insert(c)
+        pq.pqueue.insert(a)
+        pq.pqueue.insert(d)
+        pq.pqueue.insert(b)
+        assert pq.pqueue.peek() == a
